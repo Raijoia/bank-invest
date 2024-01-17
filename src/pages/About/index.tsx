d6864351c4f3edc084ad41308a6aba196ac7ef85
Raí Joia <megac154@gmail.com>
2024-01-17 10:59:54 -0300

    bank(page): create page about us

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <p>Sobre nós</p>;
+};
+
+export default About;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import PaginaPadrao from './components/PaginaPadrao';
 
+import About from './pages/About';
 import Home from './pages/Home';
 
 function AppRouter() {
@@ -10,6 +11,7 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Home />} />
+          <Route path="sobre" element={<About />} />
         </Route>
       </Routes>
     </Router>
