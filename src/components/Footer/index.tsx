da935806843cb4ca5bea34592226317195efb1b6
Raí Joia <megac154@gmail.com>
2024-01-17 10:57:50 -0300

    bank(page): create page home

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <p className="font-mono text-center">Desenvolvido por Raí.</p>
+      <p className="font-mono text-center fixed bottom-0 w-full pb-5">
+        Desenvolvido por Raí.
+      </p>
     </footer>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,15 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import PaginaPadrao from './components/PaginaPadrao';
 
+import Home from './pages/Home';
+
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<PaginaPadrao />}></Route>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
